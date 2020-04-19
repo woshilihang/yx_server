3d83a40e488b724e13d6dd586b42cbde6f14899a
李航 <2655083706@qq.com>
2020-04-19 21:33:54 +0800

    feat: 修改appid,appsecret

--- src/config/app.js
+++ src/config/app.js
@@ -1,7 +1,7 @@
 const appConf = {
   mdSecret: 'appIdSessionId',
-  appid: 'wxafc415b4df0faa5b',
-  appSecret: '200d1a12f177fe4fa7bf464e5788f26b',
+  appid: 'wxf69986470ca8008d',
+  appSecret: '2c73ade6c6b5cf07caeee90ec2004411',
   grant_type: 'authorization_code',
   grant_url: 'https://api.weixin.qq.com/sns/jscode2session',
 };
