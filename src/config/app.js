018305852d6aa1e550c2a6a8b949e2f62a4021ca
李航 <2655083706@qq.com>
2020-04-19 21:48:10 +0800

    feat: 优化

--- src/config/app.js
+++ src/config/app.js
@@ -1,7 +1,9 @@
 const appConf = {
   mdSecret: 'appIdSessionId',
-  appid: 'wxf69986470ca8008d',
-  appSecret: '2c73ade6c6b5cf07caeee90ec2004411',
+  // appid: 'wxf69986470ca8008d',
+  appid: 'wxafc415b4df0faa5b',
+  appSecret: '200d1a12f177fe4fa7bf464e5788f26b',
+  // appSecret: '2c73ade6c6b5cf07caeee90ec2004411',
   grant_type: 'authorization_code',
   grant_url: 'https://api.weixin.qq.com/sns/jscode2session',
 };
